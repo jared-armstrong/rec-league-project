597a61e44db5b9c730d70da946555872d7ddeef3
Armstrong <Jared.Armstrong@bakerhill.com>
2024-11-11 15:49:37 -0500

    added functionality to successfully submit a registrant, loaded in backend api/mongodb

--- client/rec-signups-form/src/app/app.component.ts
+++ client/rec-signups-form/src/app/app.component.ts
@@ -1,13 +1,80 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { response } from "express";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf],
+  providers: [HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'rec-signups-form';
+  private apiUrl: string = 'http://localhost:3000/api/v1/registrants'
+
+  registrant = {
+    parentInfo: {
+      parentGuardianOne: { name: '', phone: '', email: '' },
+      emergencyContact: { name: '', phone: '', email: '' },
+      physicianContact: { name: '', phone: '', email: '' },
+      addressInfo: { streetAddress: '', city: '', zipCode: '' }
+    },
+    playerInfo: [
+      {
+        name: '',
+        age: null,
+        dateOfBirth: null,
+        sport: 'baseball',
+        ageGroup: '8U',
+        shirtSize: '',
+        playsTravelBall: false,
+        waiverSigned: false,
+        medical: {
+          upToDateImmunizations: false,
+          dateOfLastTetanusShot: null,
+          allergies: '',
+          medication: '',
+          medicalConditions: ''
+        }
+      }
+    ]
+  };
+
+  constructor(private http: HttpClient) {}
+
+  addPlayer() {
+    this.registrant.playerInfo.push({
+      name: '',
+      age: null,
+      dateOfBirth: null,
+      sport: 'baseball',
+      ageGroup: '8U',
+      shirtSize: '',
+      playsTravelBall: false,
+      waiverSigned: false,
+      medical: {
+        upToDateImmunizations: false,
+        dateOfLastTetanusShot: null,
+        allergies: '',
+        medication: '',
+        medicalConditions: ''
+      }
+    });
+  }
+
+  onSubmit() {
+    this.http.post(this.apiUrl, this.registrant)
+      .subscribe({
+        error: err => {
+          console.error('Error saving registrant', err)
+        },
+        next: () => {
+          console.log('Registrant saved successfully', this.registrant);
+        }
+      });
+  }
 }
